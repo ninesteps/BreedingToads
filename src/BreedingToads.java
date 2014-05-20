bf84fcf26c328a8e014aec2ce00f11ed32d59f64
Ed-Home <bowenjuice@gmail.com>
2014-05-21 11:28:06 +1200

    Complete Rebuild

--- src/BreedingToads.java
+++ src/BreedingToads.java
@@ -9,55 +9,9 @@ import java.util.Scanner;
  */
 public class BreedingToads {
 
-    static Double[][] dist;
-    static Double[] tempDist, closest;
-    static ArrayList<Coordinate> list = new ArrayList<Coordinate>();
-    static Double size;
 
-    public static double distance(Coordinate a, Coordinate b){
-        return Math.sqrt((a.getX()-b.getX())*(a.getX()-b.getX())+(a.getY()-b.getY())*(a.getY()-b.getY()));
-    }
-
-
-    public static void sortDistances(){
-        XSmallest xsmall;
-
-        for(int i = 0; i < list.size(); i++){
-            for(int j = 0; j < list.size(); j++){
-                tempDist[j] = distance(list.get(i), list.get(j));
-            }
-            Arrays.sort(tempDist);
-            closest[i] = tempDist[15];
-        }
-        Arrays.sort(closest);
-        size = closest[15];
-
-    }
 
 
     public static void main(String args[]){
-        try{
-            Scanner filescan = new Scanner(new File(args[0])); //Try and take a file as input
-            while(filescan.hasNext()){
-                list.add(new Coordinate(filescan.nextDouble(),filescan.nextDouble()));
-            }
-            dist = new Double[list.size()][list.size()];
-            closest = new Double[list.size()];
-            tempDist = new Double[list.size()];
-
-            if(list.size() < 16){
-                System.out.println("Infinite Area / Limited Only By Size of Swamp");
-            } else {
-                sortDistances();
-                System.out.println("Biggest Diameter: " + (closest[15]-0.000000000000001)); // hax
-            }
-
-
-
-
-        } catch(FileNotFoundException e){System.err.println("File Not Found");}
-    }
-
-
 }
-
+}
